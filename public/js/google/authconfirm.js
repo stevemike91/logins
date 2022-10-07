9d051da9cea4deb5ed0428538bddd48ce6adda11
Michael <michaeljoseph6950@gmail.com>
2022-10-07 13:48:29 +0300

    ch

--- public/js/google/authconfirm.js
+++ public/js/google/authconfirm.js
@@ -24,10 +24,6 @@ const anonInvoice = document.getElementById('anon-invoice');
 const mailField = document.getElementById('exampleInputEmail');
 const signUp = document.getElementById('signUp');
 
-const signGoogle = document.getElementById("signGoogle");
-const signYahoo = document.getElementById('signYahoo');
-
-
 const phoneNumberField = document.getElementById('phoneNumber');
 const codeField = document.getElementById('code');
 const signInWithPhoneButton = document.getElementById('signInWithPhone');
@@ -190,32 +186,6 @@ $('#myform').on('submit', function(ev) {
 	$('#phoneModal').modal('hide');
 	ev.preventDefault();
 });
-
-const signInWithGoogle = () => {
-	const googleProvider = new firebase.auth.GoogleAuthProvider;
-	auth.signInWithPopup(googleProvider).then(() => {
-		sendVerificationEmail();
-		window.location.reload();
-	}).catch(error => {
-		alert(error.message)
-	});
-};
-signGoogle.addEventListener("click", signInWithGoogle);
-
-const signInWithYahoo = () => {
-	const yahooProvider = new firebase.auth.OAuthProvider('yahoo.com');
-	auth.signInWithPopup(yahooProvider).then(() => {
-		sendVerificationEmail();
-		window.location.reload();
-	}).catch(error => {
-		alert(error.message);
-	})
-}
-signYahoo.addEventListener("click", signInWithYahoo);
-
-
-
-
 
 
 jinaHolder.addEventListener("change", () => {
